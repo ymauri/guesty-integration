45468b676cb8872a2ffbfffed21cfcff2f7831db
Yolanda Mauri <ymauri@gmail.com>
2025-08-17 15:18:50 +0200

    Update sync_prices parameters in update_calendar_data function

--- app/api/v1/listeners_router.py
+++ app/api/v1/listeners_router.py
@@ -10,5 +10,5 @@ client = SyncCalendarPricesService(APIBookingExpertsClient())
 async def update_calendar_data(data: ListingCalendarUpdatedResponse):
 
     # Call the appropriate method on the client to update the calendar data
-    await client.sync_prices(1, 1, data.calendar)
+    await client.sync_prices(22671, 3114, data.calendar)
 
