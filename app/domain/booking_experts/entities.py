696fc179eaf9d1104f7334db6319257a86270afe
Yolanda Mauri <ymauri@gmail.com>
2025-09-13 11:36:08 +0200

    Add SimplePrice and ComplexPrice classes with serialization methods

---
+++ app/domain/booking_experts/entities.py
@@ -0,0 +1,35 @@
+from uuid import uuid4
+
+class SimplePrice:
+    temp_id: str
+    def __init__(self, date: str, value: float, currency: str):
+        self.date = date
+        self.currency = currency
+        self.value = value
+        self.temp_id = uuid4().hex
+
+    def to_dict(self):
+        return {
+            "temp_id": self.temp_id,
+            "date": self.date,
+            "currency": self.currency,
+            "value": self.value
+        }
+        
+class ComplexPrice:
+    temp_id: str
+    def __init__(self, arrival_date: str, value: float, currency: str, length_of_stay: int = 1):
+        self.arrival_date = arrival_date
+        self.currency = currency
+        self.value = value
+        self.length_of_stay = length_of_stay
+        self.temp_id = uuid4().hex
+
+    def to_dict(self):
+        return {
+            "temp_id": self.temp_id,
+            "arrival_date": self.arrival_date,
+            "currency": self.currency,
+            "value": self.value,
+            "length_of_stay": self.length_of_stay
+        }
